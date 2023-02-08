c2b67f57d16440095c263611b9f60ac4d0b755d5
Gene Kogan <kogan.gene@gmail.com>
2023-02-08 14:11:54 -0800

    don't save other images

--- inference_gfpgan.py
+++ inference_gfpgan.py
@@ -144,17 +144,17 @@ def main():
         for idx, (cropped_face, restored_face) in enumerate(zip(cropped_faces, restored_faces)):
             # save cropped face
             save_crop_path = os.path.join(args.output, 'cropped_faces', f'{basename}_{idx:02d}.png')
-            imwrite(cropped_face, save_crop_path)
+            #imwrite(cropped_face, save_crop_path)
             # save restored face
             if args.suffix is not None:
                 save_face_name = f'{basename}_{idx:02d}_{args.suffix}.png'
             else:
                 save_face_name = f'{basename}_{idx:02d}.png'
             save_restore_path = os.path.join(args.output, 'restored_faces', save_face_name)
-            imwrite(restored_face, save_restore_path)
+            #imwrite(restored_face, save_restore_path)
             # save comparison image
             cmp_img = np.concatenate((cropped_face, restored_face), axis=1)
-            imwrite(cmp_img, os.path.join(args.output, 'cmp', f'{basename}_{idx:02d}.png'))
+            #imwrite(cmp_img, os.path.join(args.output, 'cmp', f'{basename}_{idx:02d}.png'))
 
         # save restored img
         if restored_img is not None:
